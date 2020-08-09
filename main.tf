50e40c4ff1329b3dc2dfe5c77a6d5aec48f288d1
Vimal Paliwal <vimalpaliwal.me@gmail.com>
2020-08-09 23:33:33 +0530

    update to latest terraform syntax

--- main.tf
+++ main.tf
@@ -9,15 +9,23 @@ data "aws_route53_zone" "zone" {
 }
 
 resource "aws_route53_record" "cert_record" {
-  count   = length(var.domain_names)
-  name    = aws_acm_certificate.cert.domain_validation_options[count.index].resource_record_name
-  type    = aws_acm_certificate.cert.domain_validation_options[count.index].resource_record_type
-  zone_id = data.aws_route53_zone.zone.id
-  records = [aws_acm_certificate.cert.domain_validation_options[count.index].resource_record_value]
-  ttl     = 60
+  for_each = {
+    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  type            = each.value.type
+  zone_id         = data.aws_route53_zone.zone.id
+  records         = [each.value.record]
+  ttl             = 60
 }
 
 resource "aws_acm_certificate_validation" "cert_validation" {
   certificate_arn         = aws_acm_certificate.cert.arn
-  validation_record_fqdns = aws_route53_record.cert_record.*.fqdn
+  validation_record_fqdns = [for record in aws_route53_record.cert_record : record.fqdn]
 }
