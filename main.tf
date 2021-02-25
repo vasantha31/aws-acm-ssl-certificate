18c66ae11701249da810fd6c5944939e483b7bb8
Vimal Paliwal <vimalpaliwal.me@gmail.com>
2021-02-25 14:59:30 +0530

    bug fix

--- main.tf
+++ main.tf
@@ -32,6 +32,7 @@ resource "aws_route53_record" "cert_record" {
 }
 
 resource "aws_acm_certificate_validation" "cert_validation" {
+  count                   = var.pca_arn == null ? 0 : 1
   certificate_arn         = aws_acm_certificate.cert.arn
   validation_record_fqdns = [for record in aws_route53_record.cert_record : record.fqdn]
 }
